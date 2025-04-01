61fe86b61e5ddc77292f00c56ef1b13040355754
Linu12 <tomecki1212@gmail.com>
2025-04-01 14:32:45 +0200

    feat: add header

---
+++ src/lib/components/+header.svelte
@@ -0,0 +1,6 @@
+<header class="bg-gray-100 border-b border-gray-300 p-4">
+    <div class="container mx-auto flex items-center">
+        <img src="/images/logo.png" alt="Oliwa Wypływa Logo" class="h-12 w-12 mr-4" />
+        <h1 class="text-2xl font-bold text-gray-800">Oliwa Wypływa</h1>
+    </div>
+</header>
