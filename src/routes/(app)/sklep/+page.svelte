af818fe2278d8e3ae14879e32a5ab0955472edbb
Linu12 <tomecki1212@gmail.com>
2025-09-11 12:45:27 +0200

    feat:

--- src/routes/(app)/sklep/+page.svelte
+++ src/routes/(app)/sklep/+page.svelte
@@ -1,17 +1,13 @@
 <script>
-import InfoBannerShop from "$lib/components/shop/InfoBannerShop.svelte";
-import StarterPack from "$lib/components/+StarterPack.svelte";
-import TopProducts from "$lib/components/+TopProducts.svelte";
-import Menu from "$lib/components/shop/Menu.svelte";
-import Categories from "$lib/components/shop/Categories.svelte";
+	import StarterPack from "$lib/components/+StarterPack.svelte";
+	import TopProducts from "$lib/components/+TopProducts.svelte";
+	import Categories from "$lib/components/shop/Categories.svelte";
 </script>
 
 <svelte:head>
 	<title>OliwaWypływa</title>
 </svelte:head>
 
-<InfoBannerShop />
-<Menu />
 <Categories />
 <TopProducts />
 <StarterPack />
